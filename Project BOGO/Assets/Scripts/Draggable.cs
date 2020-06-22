324ec1d4bd7af2cdd5be768be4fbb6e0ba5dac98
Matthew <misterkarl13@gmail.com>
2020-06-21 20:08:27 -0700

    Completely revamped drag and drop logic
    Threw out the old stuff

---
+++ Project BOGO/Assets/Scripts/Draggable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform returnToMe = null;
+
+    public enum Slot { PRODUCT, COUPON };
+    public Slot typeOfItem = Slot.PRODUCT;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+
+        transform.SetParent( transform.root );
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+
+        transform.position = eventData.position;
+    }
+    
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+
+        transform.SetParent(returnToMe);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ Project BOGO/Assets/Scripts/Dropzone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Dropzone : MonoBehaviour, IDropHandler
+{
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if( d != null )
+        {
+            d.returnToMe = transform;
+        }
+    }
+}
