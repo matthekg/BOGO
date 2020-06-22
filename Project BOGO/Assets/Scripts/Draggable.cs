5ed53c694065bc7662eea8fbade6199177816a0c
Matthew <misterkarl13@gmail.com>
2020-06-22 00:15:34 -0700

    Added placeholder when card leaves a spot

--- Project BOGO/Assets/Scripts/Draggable.cs
+++ Project BOGO/Assets/Scripts/Draggable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using UnityEngine.UI;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -6,12 +7,29 @@ using UnityEngine.EventSystems;
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform returnToMe = null;
-
+    public Transform placeholderParent = null;
+    GameObject placeholder = null;
     public enum Slot { PRODUCT, COUPON };
     public Slot typeOfItem = Slot.PRODUCT;
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
+
+        placeholder = new GameObject();
+        placeholder.name = "Empty Spot";
+        placeholder.transform.SetParent(transform.parent);
+        placeholder.AddComponent<RectTransform>();
+        placeholder.GetComponent<RectTransform>().sizeDelta = new Vector2(125, 200);
+        LayoutElement layel = placeholder.AddComponent<LayoutElement>();
+        layel.preferredHeight = GetComponent<LayoutElement>().preferredHeight;
+        layel.preferredWidth = GetComponent<LayoutElement>().preferredWidth;
+        layel.flexibleHeight = 0;
+        layel.flexibleWidth = 0;
+
+        placeholder.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
+        returnToMe = transform.parent;
+        placeholderParent = returnToMe;
 
         transform.SetParent( transform.root );
 
@@ -24,6 +42,25 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         Debug.Log("OnDrag");
 
         transform.position = eventData.position;
+
+        if(placeholder.transform.parent != placeholderParent)
+            placeholder.transform.SetParent(placeholderParent);
+
+        int newSiblingIndex = placeholderParent.childCount;
+        for(int i = 0; i < placeholderParent.childCount; ++i)
+        {
+            if(transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                    --newSiblingIndex;
+
+                break;
+            }
+        }
+
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
     }
     
     public void OnEndDrag(PointerEventData eventData)
@@ -31,6 +68,8 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
         Debug.Log("OnEndDrag");
 
         transform.SetParent(returnToMe);
+        transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+        Destroy(placeholder);
 
         GetComponent<CanvasGroup>().blocksRaycasts = true;
     }
--- Project BOGO/Assets/Scripts/Dropzone.cs
+++ Project BOGO/Assets/Scripts/Dropzone.cs
@@ -3,8 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Dropzone : MonoBehaviour, IDropHandler
+public class Dropzone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+            d.placeholderParent = transform;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.placeholderParent == transform)
+            d.placeholderParent = d.returnToMe;
+    }
+
     public void OnDrop(PointerEventData eventData)
     {
         Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
